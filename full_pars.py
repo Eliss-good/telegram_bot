b525448a8fdd3aeff38244c1f7e8b5d5bdc43edd
CeplbuKot <igorcat100@gmail.com>
2022-02-12 23:13:27 +0300

    add 1 page parsr

---
+++ full_pars.py
@@ -0,0 +1,5 @@
+import pars
+
+url = 'https://mai.ru/education/studies/schedule/index.php?group=%D0%9C3%D0%9E-221%D0%91-20#'
+
+print(pars.get_prepods_from_page(url))
--- pars.py
+++ pars.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-headers = {"User-Agent": "Mozilla/5.0"}
 
-url = 'https://mai.ru/education/studies/schedule/index.php?group=%D0%9C3%D0%9E-221%D0%91-20'
+def get_prepods_from_page(url):
+    prepod_list = []
+    headers = {"User-Agent": "Mozilla/5.0"}
 
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-root = soup.html
-    
-for tag in soup.find_all("p", {"class": "mb-2 fw-semi-bold text-dark"}):
-    print("{0}: {1}".format(tag.name, " ".join(tag.text.split())))
+    for all_info in soup.find_all("ul", {"class": "list-inline list-separator text-body small"}):
+        for prepod_info in all_info.find_all("a", {"class": "text-body"}):
+            if prepod_info.text not in prepod_list:
+                prepod_list.append(prepod_info.text)
+
+    return prepod_list
