448b706eb04a839122c80ada6ff136143eca3a7c
CeplbuKot <igorcat100@gmail.com>
2022-02-20 14:19:11 +0300

    upd

--- full_pars.py
+++ full_pars.py
@@ -2,7 +2,7 @@ import prep_text_pars
 
 def parse_group(index):
     full_prep_list = []
-    begin_week = 24
+    begin_week = 1
     end_week = 30
     for week in range(begin_week, end_week):
         curr_week = prep_text_pars.get_prepods_text_list_page('https://mai.ru/education/studies/schedule/index.php?group={1}&week={0}#'.format(week, index))
@@ -13,4 +13,16 @@ def parse_group(index):
     return full_prep_list
 
 
-print(parse_group('М3О-212Б-20'))
+def parse_group_today(index):
+    full_prep_list = []
+    
+    curr_week = prep_text_pars.get_prepods_text_list_page('https://mai.ru/education/studies/schedule/index.php?group={0}#'.format(index))
+    for prep in curr_week:
+        if prep not in full_prep_list:
+            full_prep_list.append(prep)
+
+    return full_prep_list
+
+
+# print(parse_group('М3О-221Б-20'))
+print(parse_group_today('М3О-221Б-20'))
--- prep_text_pars.py
+++ prep_text_pars.py
@@ -1,3 +1,4 @@
+from operator import le
 import requests
 from bs4 import BeautifulSoup
 
@@ -13,10 +14,19 @@ def get_prepods_text_list_page(url):
 
     for select_div in soup.find_all("div", {"class": "mb-4"}):
         for select_lesson in select_div.find_all("p", {"class": "mb-2 fw-semi-bold text-dark"}):
-            lesson = (" ".join(select_lesson.text.split()))
+            lesson = " ".join(select_lesson.find(text=True, recursive=False).split())
+            
+            for select_lesson_next in select_lesson.find_all("span", {"class": "text-nowrap"}):
+                lesson_part = " ".join(select_lesson_next.find(text=True, recursive=False).split())
+                full_lesson = lesson + " " + lesson_part
+                
+                for lesson_type in select_lesson_next.find_all("span", {"class": "badge bg-soft-secondary text-secondary ms-2 fw-medium text-smaller"}):
+                    type = " ".join(lesson_type.find(text=True, recursive=False).split())    
+                    
         for select_prepod in select_div.find_all("a", {"class": "text-body"}):
             if {"prepod": select_prepod.text, "lesson": lesson} not in prepod_list:
                 prepod_list.append(
-                    {"prepod": select_prepod.text, "lesson": lesson})
+                    {"prepod": select_prepod.text, "lesson": full_lesson, "role": type})
 
     return(prepod_list)
+
