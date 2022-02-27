1958552a8e0d8c7449ca57b8daf9e60ce2a813af
Eliss-good <elisssupergames@gmail.com>
2022-02-27 11:31:29 +0300

    eliss: fix bd

--- db_setting/tg_connect_db.py
+++ db_setting/tg_connect_db.py
@@ -1,23 +1,7 @@
 import us_init
-import json
 
-
-def _group_cheack_approved(name_group):
-    with open('approved_group.json','r', encoding='utf-8') as file:
-        apr_group = json.load(file)
-
-        try:
-            status = apr_group[name_group]
-            return status
-        except KeyError:
-            return False
-
-
-def jump_group_prepod(name_prepod):
-    data = us_init.fl.parse_prepod(name_prepod)
-
-    for item in data:
-        us_init.data_for_prepod(item)
+def poll_strat():
+    pass
 
 
 def student_start(name_user, id_us_tg,name_group):
@@ -28,10 +12,8 @@ def student_start(name_user, id_us_tg,name_group):
 
 def prepod_start(name_user, id_us_tg):
     us_init.add_prepod(name_user, id_us_tg)
-    
-    
+    us_init.start_pr(name_user)
 
-    
 
 def reg_us(name_user, id_us_tg, role, group_stud = None):
     us_init.add_us(id_us_tg, role)
