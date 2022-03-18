01a70c320d13f1f13885dd96527d87a95b7ad331
CepbluKot <awesomecosmonaut@gmail.com>
2022-03-18 17:03:33 +0300

    add storages

--- bot_elements/setter/all_setters.py
+++ bot_elements/setter/all_setters.py
@@ -1,11 +1,11 @@
-from bot_elements.forms import temp_form_recipient_data 
-from bot_elements.forms import temp_mem_for_form_creator
-from bot_elements.forms import mem_for_created_forms
-from bot_elements.forms import send_forms_mem 
+from bot_elements.storages.all_storages import temp_form_recipient_data 
+from bot_elements.storages.all_storages import temp_mem_for_form_creator
+from bot_elements.storages.all_storages import mem_for_created_forms
+from bot_elements.storages.all_storages import send_forms_mem 
 # temp_mem_for_form_creator + temp_poll_recip_data -> mem_for_created_forms -> send_forms_mem -> completing_forms_dispatcher
 
-from bot_elements.forms import completing_forms_dispatcher # {'user_id': {''unique_form_id'': id, 'unique_sent_form_id': id, 'curr_page': num, 'form_copy': [form_data]}, ...}
-from bot_elements.register import registerData # {*user_id*: {'user_role': *role*, 'user_group': *group*}}
+from bot_elements.storages.all_storages import completing_forms_dispatcher # {'user_id': {''unique_form_id'': id, 'unique_sent_form_id': id, 'curr_page': num, 'form_copy': [form_data]}, ...}
+from bot_elements.storages.all_storages import registerData # {*user_id*: {'user_role': *role*, 'user_group': *group*}}
 
 
 def temp_form_recipient_data_add_user_data(chat_id: int, form_name: str, type: str, form_id: int, creator_id: int):
