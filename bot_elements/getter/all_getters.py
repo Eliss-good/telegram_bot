01904dda7801fa0f7a426009b7160ab433e5038a
CepbluKot <igorcat100@gmail.com>
2022-03-24 12:43:22 +0300

    udp getters

--- bot_elements/getter/all_getters.py
+++ bot_elements/getter/all_getters.py
@@ -42,7 +42,7 @@ def completing_forms_dispatcher_get_form_copy(user_id: int):
 
 
 def completing_forms_dispatcher_get_form_question_message_id(user_id: int, question_num: int):
-    """ Возвращает копию формы из completing_forms_dispatcher"""
+    """ Возвращает айди сообщения(опроса) по номеру вопроса из completing_forms_dispatcher"""
     return completing_forms_dispatcher[user_id]['form_copy'][question_num]['message_id']
 
 
@@ -52,13 +52,8 @@ def completing_forms_dispatcher_get_current_question_num(user_id: int):
 
 
 def completing_forms_dispatcher_get_question_by_num(user_id: int, question_num: int):
-    """ Возвращает номер текущего вопроса из completing_forms_dispatcher"""
+    """ Возвращает вопрос по номеру вопроса из completing_forms_dispatcher"""
     return completing_forms_dispatcher[user_id]['form_copy'][question_num]
-
-
-def completing_forms_dispatcher_get_question_id(user_id: int, question_num: int):
-    """ Возвращает номер текущего вопроса из completing_forms_dispatcher"""
-    return completing_forms_dispatcher[user_id]['form_copy'][question_num]['message_id']
 
 
 def registerData_get_fio(user_id: int):
@@ -77,8 +72,9 @@ def registerData_get_role(user_id: int):
 
 
 def registerData_check_is_registered(user_id: int):
-    """ Возвращает роль юзера из registerData"""
+    """ Проверяет есть ли юзер в registerData"""
     return user_id in registerData.keys()
 
 def send_forms_mem_get():
+    """ Возвращает send_forms_mem (память с отправленынми формами)"""
     return send_forms_mem
--- bot_elements/setter/all_setters.py
+++ bot_elements/setter/all_setters.py
@@ -18,7 +18,7 @@ def temp_form_recipient_data_add_user_data(chat_id: int, form_name: str, type: s
 
 
 def temp_mem_for_form_creator_add_element(user_id: int, data: dict):
-    """Добавляет 1 элемент формы во временный словарь для создания формы"""
+    """ Добавляет 1 элемент формы во временный словарь для создания формы"""
     if user_id in temp_mem_for_form_creator:
             temp_mem_for_form_creator[user_id].append(data)
     else:
@@ -26,7 +26,7 @@ def temp_mem_for_form_creator_add_element(user_id: int, data: dict):
 
 
 def mem_for_created_forms_add_element(user_id: int, data):
-    """Добавляет 1 форму в словарь сохраненных форм"""
+    """ Добавляет 1 форму в словарь сохраненных форм"""
     mem_for_created_forms[user_id] = data
 
 
@@ -51,18 +51,18 @@ def mem_for_created_forms_edit_poll_options(form_id: int, question_id: int, new_
 
 
 def send_forms_mem_add_sent_form(form_id: int, sent_form_id: int, form_creator_user_id: int, send_to_users_ids: list):
-    """Добавляет 1 форму в список с отправленными формами"""
+    """ Добавляет 1 форму в список с отправленными формами"""
     send_forms_mem.append({'form_id': form_id, 'sent_form_id': sent_form_id, 'info': {'form_creator_user_id': form_creator_user_id, 'send_to_users_ids': send_to_users_ids}})
 
 
 def completing_forms_dispatcher_add_session(chat_id: int, unique_form_id: int, unique_sent_form_id: int):
-    """Добавляет 1 сессию в список активных сессий"""
+    """ Добавляет 1 сессию в список активных сессий"""
     completing_forms_dispatcher[chat_id] = {
         'chat_id': chat_id, 'unique_form_id': unique_form_id, 'unique_sent_form_id': unique_sent_form_id, 'current_question_num': 0,'form_copy': mem_for_created_forms[unique_form_id]}
     
 
 def completing_forms_dispatcher_add_1_to_question_num(user_id: int):
-    """Увеличивает на 1 номер вопроса"""
+    """ Увеличивает на 1 номер текущего задаваемого вопроса"""
     completing_forms_dispatcher[user_id]['current_question_num'] += 1
 
 
@@ -73,15 +73,15 @@ def completing_forms_dispatcher_set_question_id(user_id: int, question_num: int,
 
 
 def registerData_add_user(user_id: int, chosen_fio: str, chosen_group: str, chosen_role: str):
-    """Добавляет рег. данные пользователя"""
+    """ Добавляет рег. данные пользователя"""
     registerData[user_id] = {'chosen_fio': chosen_fio, 'chosen_group': chosen_group, 'chosen_role': chosen_role}
 
 
 def registerData_change_group_data(user_id: int, new_group: str):
-    """Изменяет группу пользователя"""
+    """ Изменяет группу пользователя"""
     registerData[user_id]['chosen_group'] = new_group
     
 
 def registerData_change_fio_data(user_id: int, new_fio: str):
-    """Изменяет ФИО пользователя"""
+    """ Изменяет ФИО пользователя"""
     registerData[user_id]['chosen_fio'] = new_fio
